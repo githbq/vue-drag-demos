5f3f44a5c86c4729b9cf50066765ad7c28df496b
hubiqing <hbqhbq123@qq.com>
2020-04-28 10:56:13 +0800

    ok

---
+++ src/views/drag-demo1.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="drag-demo1">
+    <h1>drag-demo1</h1>
+  </div>
+</template>
---
+++ src/views/drag-demo2.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="drag-demo2">
+    <h1>drag-demo2</h1>
+  </div>
+</template>
